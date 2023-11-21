b3c3b1d532abf3f8d926c97de1489cfa4919f42f
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-11-21 20:03:56 +0530

    flash

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index');
 });
+//With the use of flash we can use the data of a router in another routes.
+
+router.get('/shibhu', function(req, res){
+  req.flash("age", 26);
+  res.send("Age save ho gyi");
+});
+
+router.get('/check', function(req, res){
+  console.log(req.flash("age"));
+  res.send("check kro terminal")
+})
 
 module.exports = router;
