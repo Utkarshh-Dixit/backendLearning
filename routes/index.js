5e888a37ca0e9f7d8c0d566b790de3fe1f9d3797
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2023-11-23 15:31:15 +0530

    finding data in database

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
-});
-//With the use of flash we can use the data of a router in another routes.
+const userModel = require("./users");
 
-router.get('/shibhu', function(req, res){
-  req.flash("age", 26);
-  res.send("Age save ho gyi");
-});
+router.get('/', function(req, res){
+  res.send("This is the home page");
+})
 
-router.get('/check', function(req, res){
-  console.log(req.flash("age"));
-  res.send("check kro terminal")
+// router.get('/create', async function(req, res){
+//   const createdUser = await userModel.create({
+//     username: "Kush",
+//     nickname: "Rinku",
+//     description: "Not interested in coding and an unlimited studious person",
+//     categories: ['css', 'java']
+//   });
+//   res.send(createdUser);
+// })
+
+router.get('/find', async function(req, res){
+  let king = await userModel.find({username: "utkarsh"});
+
+  res.send(king);
 })
 
 module.exports = router;
+/* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index');
+// });
+// //With the use of flash we can use the data of a router in another routes.
+
+// router.get('/shibhu', function(req, res){
+//   req.flash("age", 26);
+//   res.send("Age save ho gyi");
+// });
+
+// router.get('/check', function(req, res){
+//   console.log(req.flash("age"));
+//   res.send("check kro terminal")
+// })
